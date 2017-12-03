c86d5f471d2a9bccdcb4b6c14f76609598f2342c
Zack <zack.newcomb1@gmail.com>
2017-12-03 01:52:29 -0500

    Updated player.cpp
    In the team.cpp, instead of printing two in the DisplayPlayer and PrintTeam, I migrated that code to the player.cpp. Now I can invoke that function from my player class instead.

--- player.cpp
+++ player.cpp
@@ -58,3 +58,39 @@ int player::get_calc_perc2(int c, int d) {
 int player::get_calc_percft(int e, int f) {
 	return lst_ft[num_games_played].calc_perc(e, f);
 }
+void player::print() {
+		cout << name << " [" << jersey_num << "]\n";
+		cout << left
+			<< setw(4) << "Game" << "  "
+			<< setw(12) << "3-Point FGs" << "  "
+			<< setw(12) << "2-Point FGs" << "  "
+			<< setw(12) << "Free Throws" << "  "
+			<< setw(5) << "Total" << endl;
+		cout << "----  ------------  ------------  ------------  -----" << endl;
+		int full_games = num_games_played;
+		for (int g = 0; g <= full_games; g++) {
+			num_games_played = g;
+			int a3 = get_3att();
+			int m3 = get_3made();
+			int a2 = get_2att();
+			int m2 = get_2made();
+			int mft =get_ftmade();
+			int aft =get_ftatt();
+
+			cout << right;
+
+			string str_game = (to_string(g+1));
+			string str_3pt = (to_string(m3) + "/" + to_string(a3) + " (" +to_string(get_calc_perc3(a3, m3)) + "%)");
+			string str_2pt = (to_string(m2) + "/" + to_string(a2) + " (" + to_string(get_calc_perc2(a2, m2)) + "%)");
+			string str_ft = (to_string(mft) + "/" + to_string(aft) + " (" + to_string(get_calc_percft(aft, mft)) + "%)");
+			string str_total = (to_string((m3 * 3) + (m2 * 2) + (mft)));
+			cout << fixed << setprecision(2);
+			cout << setw(4) << str_game << "  ";
+			cout << setw(12) << str_3pt << "  ";
+			cout << setw(12) << str_2pt << "  ";
+			cout << setw(12) << str_ft << "  ";
+			cout << setw(5) << str_total;
+			cout << "\n";
+
+		} cout << "\n"; num_games_played = full_games;
+	} 
